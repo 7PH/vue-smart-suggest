f326a8ed9fabf59f86d1306ae1b777dd95ee83ed
7PH <b.raymond@protonmail.com>
2026-09-02 10:46:15 +0200

    Add examples page with a link from the demo section

--- src/docs/router.ts
+++ src/docs/router.ts
@@ -1,11 +1,13 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import HomeView from './views/HomeView.vue';
 import CustomizeView from './views/CustomizeView.vue';
+import ExamplesView from './views/ExamplesView.vue';
 
 export const router = createRouter({
     history: createWebHashHistory(),
     routes: [
         { name: 'home', path: '/', component: HomeView },
+        { name: 'examples', path: '/examples', component: ExamplesView },
         { name: 'customize', path: '/customize', component: CustomizeView },
     ],
 });
